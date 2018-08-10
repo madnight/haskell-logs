6c6dea8027abbfbebecf48b0eb3ba5f2336acb76
Fabian Beuke <mail@beuke.org>
2018-08-11 01:05:43 +0200

    refactoring

--- Main.hs
+++ Main.hs
@@ -1,73 +1,78 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE DeriveGeneric       #-}
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE PatternSynonyms     #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE LambdaCase                #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE PatternSynonyms           #-}
+{-# LANGUAGE RecordWildCards           #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeOperators             #-}
 
 module Main where
 
-import           Control.Monad
-import           Data.Aeson
+import           Control.Monad                 (unless)
+import           Data.Aeson                    (eitherDecodeStrict)
 import           Data.Aeson.Types
 import           Data.List                     (intercalate, isInfixOf)
 import           Data.Map                      (fromList)
-import           Data.Maybe
-import           Data.Proxy
+import           Data.Maybe                    (isJust)
 import           GHC.Generics
-import           GHCJS.Foreign.Callback
-import           GHCJS.Types
+import           GHCJS.Types                   (JSString)
 import           JavaScript.Web.XMLHttpRequest
 import           Miso                          hiding (defaultOptions)
 import qualified Miso.String                   as M
 import           Network.URI                   hiding (query)
-import           Servant.API                   hiding (GET)
-import           Servant.Utils.Links
-
-maxIndexSize :: Integer
-maxIndexSize = 151275
 
 foreign import javascript safe
     " setTimeout(                             \
     \   function() {                          \
     \    var e = document.getElementById($1); \
     \    if (e) e.scrollIntoView();           \
-    \   }, 500);"
+    \   }, 500);                              "
    scrollIntoView :: JSString -> IO ()
 
 -- | Model
 data Model =
-  Model { info  :: Maybe APIInfo
-        , index :: Maybe APIInfo
-        , uri   :: URI
+  Model { searchResult  :: Maybe APIResult
+        , index         :: Maybe APIResult
+        , uri           :: URI
          -- ^ current URI of application
-        , query :: M.MisoString
+        , query         :: M.MisoString
         } deriving (Eq, Show)
 
 -- | Action
-data Action =
-    FetchGitHub M.MisoString
-  | SetGitHub APIInfo
-  | SetQuery M.MisoString
-  | FetchIndex M.MisoString
-  | SetIndex APIInfo
-  | HandleURI URI
-  | ChangeURI URI
-  | NoOp
+data Action where
+    QueryAPI    :: Action
+    SetAPIResponse   :: APIResult    -> Action
+    SetQuery    :: M.MisoString -> Action
+    SetIndex    :: APIResult    -> Action
+    HandleURI   :: URI          -> Action
+    ChangeURI   :: URI          -> Action
+    NoOp        :: Action
   deriving (Show, Eq)
+
+data APIResult = APIResult
+  { database :: M.MisoString
+  , rows     :: [[M.MisoString]]
+  , query_ms :: Float
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON APIResult where
+  parseJSON = genericParseJSON
+    defaultOptions { fieldLabelModifier = camelTo '_' }
 
 -- | Main entry point
 main :: IO ()
 main = do
    currentURI <- getCurrentURI
-   startApp App { model = Model { info = Nothing
-                                , index = Nothing
-                                , query = ""
-                                , uri = currentURI
-                                }
+   startApp App { model =
+                    Model { searchResult = Nothing
+                          , index = Nothing
+                          , query = ""
+                          , uri = currentURI
+                          }
                 , initialAction = NoOp
                 , mountPoint = Nothing
                 , ..
@@ -80,23 +85,29 @@ main = do
       mountPoint = Nothing
 
 
+maxIndexSize :: Integer
+maxIndexSize = 151275
+
+fMS :: M.ToMisoString str => M.MisoString -> str
+fMS = M.fromMisoString
+
+pattern EnterButton :: KeyCode
+pattern EnterButton = KeyCode 13
+
 -- | Update your model
 updateModel :: Action -> Model -> Effect Action Model
-
-updateModel (FetchIndex s) m = m <# do
-  SetIndex <$> queryLogDatabase (query m)
 
 updateModel (SetIndex apiInfo) m =
   noEff m { index = Just apiInfo }
 
-updateModel (FetchGitHub s) m = m <# do
-  SetGitHub <$> queryLogDatabase (query m)
+updateModel QueryAPI m = m <# do
+  SetAPIResponse <$> queryLogDatabase (query m)
 
 updateModel (SetQuery q) m =
   noEff m { query = q }
 
-updateModel (SetGitHub apiInfo) m =
-  noEff m { info = Just apiInfo }
+updateModel (SetAPIResponse apiInfo) m =
+  noEff m { searchResult = Just apiInfo }
 
 updateModel (ChangeURI u) m = m <# do
   pushURI u
@@ -105,8 +116,7 @@ updateModel (ChangeURI u) m = m <# do
 updateModel (HandleURI u) m = m { uri = u } <# do
   let pager = read (last $ pathSegments u) :: Integer
   let anchor = uriFragment u
-  let query = M.toMisoString $
-          intercalate "+"
+  let query = M.toMisoString $ intercalate "+"
           ["rowid", ">"
           , show (pager * 100)
           , "AND"
@@ -135,8 +145,8 @@ clickGoto' str = onClick $ goto $ "/haskell-logs/index/" ++ str
 emptyDiv :: View action
 emptyDiv = div_ [] []
 
-pagLink :: Integer -> View Action
-pagLink i
+pgLink :: Integer -> View Action
+pgLink i
   | i < 0 || i > maxIndexSize = emptyDiv
   | otherwise = li_
     [clickGoto i]
@@ -145,28 +155,13 @@ pagLink i
         [text . M.pack $ show i]
     ]
 
-pagLinkLeft :: Integer -> Integer -> View Action
-pagLinkLeft i p | i < 3     = emptyDiv
-                | otherwise = pagLink p
-
-pagLinkRight :: Integer -> Integer -> View Action
-pagLinkRight i p | i > (maxIndexSize - 3) = emptyDiv
-                 | otherwise              = pagLink p
-
 dots :: View action
 dots = li_ [] [span_
               [class_ $ M.pack "pagination-ellipsis" ]
               [text $ M.pack "..."]]
 
-pagDotsLeft :: Integer -> View Action
-pagDotsLeft i = if i < 3 then emptyDiv else dots
-
-pagDotsRight :: Integer -> View Action
-pagDotsRight i = if i > maxIndexSize - 3 then emptyDiv else dots
-
-pagCurrent :: Integer -> View Action
-pagCurrent i = i_
-  []
+pgCurrent :: Integer -> View Action
+pgCurrent i = i_ []
   [ a_
       [ class_ $ M.pack "pagination-link is-current"
       , textProp "aria-label" $ M.pack ""
@@ -175,68 +170,68 @@ pagCurrent i = i_
       [text . M.pack $ show i]
   ]
 
-pagPrevNext :: Bool -> Integer -> String -> M.MisoString -> View Action
-pagPrevNext True i a b =
+pgPrevNext :: String -> M.MisoString -> Integer -> Bool -> View Action
+pgPrevNext a b i True =
   a_ [class_ $ M.pack a, clickGoto i, disabled_ True] [text b]
-pagPrevNext False i a b = a_ [class_ $ M.pack a, clickGoto i] [text b]
-
-pagPrev :: Integer -> View Action
-pagPrev index = pagPrevNext (index < 0) index "pagination-previous" "Previous"
-
-pagNext :: Integer -> View Action
-pagNext index =
-   pagPrevNext (index > maxIndexSize) index "pagination-next" "Next Page"
+pgPrevNext a b i False = a_ [class_ $ M.pack a, clickGoto i] [text b]
 
 notFound404 :: View Action
 notFound404 =
-  div_ [] [text "the 404 :(", button_ [onClick goHome] [text "go home Y"]]
-
-onLoaded :: action -> Attribute action
-onLoaded action = on "onload" emptyDecoder $ \() -> action
+  div_ [] [text "the 404 :(", button_ [onClick $ goto "/haskell-logs"]
+  [text "go home Y"]]
 
 headerStyle :: Attribute action
 headerStyle = style_ $ fromList [
-                  (M.pack "text-align", M.pack "center")
-                , (M.pack "margin", M.pack "50px")
-                ]
+      (M.pack "text-align", M.pack "center")
+    , (M.pack "margin", M.pack "50px")]
 
 header :: String -> View action
 header s = h1_ [class_ $ M.pack "title"] [text $ M.pack s]
 
-indexView :: Model -> View Action
-indexView model@Model {..} =
+pagination :: Model -> View Action
+pagination model@Model {..} =
   let pager = read (last $ pathSegments uri) :: Integer
+      if' p t f = if p then t else f
+      pgNext = pgPrevNext "pagination-next" "Next Page" <*> (> maxIndexSize)
+      pgPrev = pgPrevNext "pagination-previous" "Previous" <*> (< 0)
+      pgDotsRight i = if   i > maxIndexSize - 3 then emptyDiv else dots
+      pgLinkRight i = if' (i > (maxIndexSize - 3)) emptyDiv . pgLink
+      pgDotsLeft  i = if   i < 3 then emptyDiv else dots
+      pgLinkLeft  i = if' (i < 3) emptyDiv . pgLink
   in
-    div_
-      [ headerStyle ]
-      [ header "Haskell IRC Log Index"
-      , nav_
+    nav_
         [ class_ $ M.pack "pagination"
         , textProp "role" $ M.pack "navigation"
         , textProp "aria-label" $ M.pack "pagination"
         ]
-        [ pagPrev $ pager - 1
-        , pagNext $ pager + 1
+        [ pgPrev $ pager - 1
+        , pgNext $ pager + 1
         , ul_
           [class_ $ M.pack "pagination-list"]
-          [ pagLinkLeft pager 0
-          , pagDotsLeft pager
-          , pagLink $ pager - 2
-          , pagLink $ pager - 1
-          , pagCurrent $ pager + 0
-          , pagLink $ pager + 1
-          , pagLink $ pager + 2
-          , pagDotsRight pager
-          , pagLinkRight pager maxIndexSize
+          [ pgLinkLeft pager 0
+          , pgDotsLeft pager
+          , pgLink $ pager - 2
+          , pgLink $ pager - 1
+          , pgCurrent $ pager + 0
+          , pgLink $ pager + 1
+          , pgLink $ pager + 2
+          , pgDotsRight pager
+          , pgLinkRight pager maxIndexSize
           ]
         ]
+
+indexView :: Model -> View Action
+indexView model@Model {..} =
+  let pager = read (last $ pathSegments uri) :: Integer
+      onLoaded = on "onload" emptyDecoder . const
+  in
+    div_
+      [ headerStyle ]
+      [ header "Haskell IRC Log Index"
+      , pagination model
       , case index of
-        Nothing           -> div_ [] [text $ M.pack "Loading ...."]
-        Just APIInfo {..} -> div_
-          []
-          [ br_ [] []
-          , th_
-            []
+        Nothing             -> div_ [] [text $ M.pack "Loading ...."]
+        Just APIResult {..} -> div_ [] [ br_ [] [] , th_ []
             [ text $ M.pack
                 (  "Log Entries ["
                 ++ show (pager * 100)
@@ -248,26 +243,40 @@ indexView model@Model {..} =
           , th_ [] []
           , table_
             [class_ $ M.pack "table is-striped"]
-            [ thead_ [] [ td_ [] [i] | i <- ["Date", "Time", "User", "Post"] ]
+            [ thead_ [] [ td_ [] [i] | i <- ["Date", "Time", "User", "Post"]]
             , tbody_ [onLoaded $ HandleURI uri] $ results_ rows
             ]
           ]
       ]
 
+searchInput :: Model -> View Action
+searchInput model@Model {..} = input_ ([ onKeyDown $ \case
+         EnterButton -> QueryAPI
+         _           -> NoOp
+       , onInput (\x -> SetQuery $ M.toMisoString $ intercalate "+"
+           [ "post"
+           , "like"
+           , "\"%"
+           , fMS x :: String
+           , "%\""
+           , "limit"
+           , "14"
+           ]
+         )
+       , autofocus_ True
+       , class_ $ M.pack "button is-large is-outlined"
+       ] ++ [ disabled_ False | isJust searchResult ])
+       [text $ M.pack ""]
 
 searchView :: Model -> View Action
 searchView model@Model {..} =
   div_
     [ headerStyle ]
     [ header "Haskell IRC Log Search"
-  , input_ attrs [text $ M.pack ""]
-  , case info of
-    Nothing           -> div_ [] [text $ M.pack ""]
-    Just APIInfo {..} -> div_
-      []
-      [ br_ [] []
-      , th_
-        []
+  , searchInput model
+  , case searchResult of
+    Nothing             -> div_ [] [text $ M.pack ""]
+    Just APIResult {..} -> div_ [] [ br_ [] [] , th_ []
         [text $ M.pack ("Search Results (" ++ show (round query_ms) ++ " ms)")]
       , table_
         [class_ $ M.pack "table is-striped"]
@@ -276,70 +285,40 @@ searchView model@Model {..} =
         ]
       ]
   ]
- where
-   attrs =
-     [ onKeyDown $ \case
-         EnterButton -> FetchGitHub "  "
-         _           -> NoOp
-       , onInput
-         (\x -> SetQuery $ M.toMisoString $ intercalate
-           "+"
-           [ "post"
-           , "like"
-           , "\"%"
-           , M.fromMisoString x :: String
-           , "%\""
-           , "limit"
-           , "14"
-           ]
-         )
-       , autofocus_ True
-       , class_ $ M.pack "button is-large is-outlined"
-       ]
-       ++ [ disabled_ False | isJust info ]
 
 viewModel :: Model -> View Action
 viewModel model@Model {..}
     | "index" `isInfixOf` show uri = indexView model
     | otherwise = searchView model
 
-pattern EnterButton :: KeyCode
-pattern EnterButton = KeyCode 13
-
-pager :: M.MisoString -> String
-pager s =
-  show ((read (M.fromMisoString s) :: Integer) `div` 100)
-    ++ "#"
-    ++ M.fromMisoString s
-
 results_ :: [[M.MisoString]] -> [View Action]
 results_ ((a:b:c:d:e):xs) =
-  tr_ [id_ (M.fromMisoString a), clickGoto' $ pager a]
-      (tdd $ fromatRow (a:b:c:d:e))
+ let formatRow (a:b:c:d:e) = b:c: M.concat ["<", d, ">"] :e
+ in tr_ [id_ (fMS a), clickGoto' $ pager a]
+      (tdd $ formatRow (a:b:c:d:e))
     : results_ xs
+ where
+   pager s = show ((read (fMS s) :: Integer) `div` 100) ++ "#" ++ fMS s
+   tdd (x:xs) = td_ [] [text x] : tdd xs
+   tdd _      = []
 results_ _ = []
 
-fromatRow :: [M.MisoString] -> [M.MisoString]
-fromatRow (a:b:c:d:e) = b:c: M.concat ["<", d, ">"] :e
+parseURI :: String -> URI
+parseURI =  uriparser . parseRelativeReference
+ where
+   uriparser (Just x) = x
+   uriparser Nothing = error "404"
 
-tdd :: [M.MisoString] -> [View action]
-tdd (x:xs) = td_ [] [text x] : tdd xs
-tdd _      = []
+goto :: String -> Action
+goto = ChangeURI . uriparser . parseRelativeReference
+ where
+   uriparser (Just x) = x
+   uriparser Nothing = error "404"
 
-data APIInfo = APIInfo
-  { database :: M.MisoString
-  , rows     :: [[M.MisoString]]
-  , query_ms :: Float
-  } deriving (Show, Eq, Generic)
-
-instance FromJSON APIInfo where
-  parseJSON = genericParseJSON
-    defaultOptions { fieldLabelModifier = camelTo '_' }
-
-queryLogDatabase :: M.MisoString -> IO APIInfo
+queryLogDatabase :: M.MisoString -> IO APIResult
 queryLogDatabase q = do
   Just resp <- contents <$> xhrByteString req
-  case eitherDecodeStrict resp :: Either String APIInfo of
+  case eitherDecodeStrict resp :: Either String APIResult of
     Left  s -> error s
     Right j -> pure j
  where
@@ -357,31 +336,10 @@ queryLogDatabase q = do
       , "from"
       , "db"
       , "where"
-      , M.fromMisoString q
+      , fMS q
       ]
     , reqLogin           = Nothing
     , reqHeaders         = []
     , reqWithCredentials = False
     , reqData            = NoData
     }
-
-
--- | HasURI typeclass
-instance HasURI Model where
-  lensURI = makeLens getter setter
-    where
-      getter = uri
-      setter m u = m { uri = u }
-
-uriparser :: Maybe t -> t
-uriparser (Just x) = x
-
-goto :: String -> Action
-goto = ChangeURI . uriparser . parseRelativeReference
-
-goAbout :: Action
-goAbout = goto "/haskell-logs/index"
-
-goHome  :: Action
-goHome = goto "/haskell-logs"
-
